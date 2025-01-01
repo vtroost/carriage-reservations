c716673e564bd9284c667c38cd5a05157acd8041
vtroost <34439781+vtroost@users.noreply.github.com>
2025-01-01 22:22:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import BookingForm from './components/BookingForm';
 
-const App = () => <BookingForm />;
+function App() {
+  return <BookingForm />;
+}
+
 export default App;
